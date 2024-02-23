9f62fab18077b0166fdb72958626a2ebb842e2e3
Peter Nydahl <peter.nydahl@iths.se>
2024-02-23 15:08:23 +0100

    Innan migration som lägger in order tabell i databasen

---
+++ NydahlsWebshop1000/NydahlsWebshop1000/Models/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace NydahlsWebshop1000.Models;
+public class Order
+{
+    public int Id { get; set; }
+    public List<Product>? OrderedProducts { get; set; }
+    public int CustomerId { get; set; }
+    public long Price { get; set; }
+}
